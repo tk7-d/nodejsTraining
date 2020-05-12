cdc5363afffac8225ddcbc9cbf56c51cedb06588
tk7-d <62370442+tk7-d@users.noreply.github.com>
2020-05-13 02:11:26 +0530

    Day 5 exercise

--- Day 5/web-app/index.js
+++ Day 5/web-app/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const server = express();
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
 
 server.get('/', (req, res) => {
     res.send('Welcome to web app');
-})
+});
+
+server.post('/', (req, res) => {
+    var dateTime = JSON.stringify(new Date(req.body.time * 1000)).split("T");
+    var day = dateTime[0].substring(1);
+    var time = dateTime[1].substring(0,5);
+    res.send(day + " " + time);
+});
 
 server.listen(4242, () => {
     console.log("express server is running");
